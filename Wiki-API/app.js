c25c65b064d4a5494b59e36b08d0985db741f410
Bagla, Roshan <Roshan.Bagla@tabcorp.com.au>
2020-06-20 22:30:46 +1000

    Created my first RESTful API

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -24,49 +24,116 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-// get request to fetch all the articles. (GET)
-app.get('/articles', function (req, res) {
-    //query our database
-    Article.find(function (error, foundArticles) {
-        if (error) {
-            res.send(error);
-        } else {
-            res.send(foundArticles);
-        }
+// ********************* Request targetting ALL Articles**************************************
+
+// chaining all my methods using route 
+app.route("/articles")
+
+    // get request to fetch all the articles. (GET)
+    .get(function (req, res) {
+        //query our database
+        Article.find(function (error, foundArticles) {
+            if (error) {
+                res.send(error);
+            } else {
+                res.send(foundArticles);
+            }
+        })
     })
-});
 
-//Sending a POST request using POSTMAN  (Create)
+    //Sending a POST request using POSTMAN  (Create)
+    .post(function (req, res) {
 
-app.post('/articles', function (req, res) {
-    console.log(req.body.title)
-    console.log(req.body.content)
+        //insert the post request data into MongoDB database
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save(function (err) {
+            if (err) {
+                console.log("Error occured");
+            } else {
+                res.send("Article has been inserted successfully");
+            }
+        });
+    })
 
-    //insert the post request data into MongoDB database
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all the articles");
+            } else {
+                res.send(err);
+            }
+        });
     });
-    newArticle.save(function (err) {
-        if (err) {
-            console.log("Error occured");
-        } else {
-            res.send("Article has been inserted successfully");
-        }
-    });
-});
 
-// deleting data from our database
+// ********************* Request targetting a specific Article**************************************
 
-app.delete("/articles", function (req, res) {
-    Article.deleteMany(function(err) {
-        if (!err) {
-            res.send("Successfully deleted all the articles");  
-        } else {
-            res.send(err);
-        }
+//localhost:3000/articles/Jack-Bauer
+
+
+app.route("/articles/:articleTitle")
+    .get(function (req, res) {
+        console.log("You are looking for:", req.params.articleTitle);
+        Article.findOne({
+            title: req.params.articleTitle
+        }, function (err, foundArticles) {
+            if (foundArticles) {
+                res.send(foundArticles);
+            } else {
+                res.send("No  articles found");
+            }
+        })
+    })
+
+    // update the enitre field in DB.
+    .put(function (req, res) {
+        Article.update({
+                title: req.params.articleTitle // look for the title 
+            }, { // replace the title with the req data
+                title: req.body.title,
+                content: req.body.content
+            }, {
+                overwrite: true
+            },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated articles");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
+    })
+    // update a specific field in DB
+    .patch(function (req, res) {
+        Article.update({
+                title: req.params.articleTitle
+            }, {
+                $set: req.body
+            },
+            function (err) {
+                if (!err) {
+                    res.send("Sucessfully updated the data");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
+    })
+
+    .delete(function (req, res) {
+        Article.deleteOne({
+            title: req.params.articleTitle
+        }, function (err) {
+            if (!err) {
+                res.send("Data has been deleted");
+            } else {
+                res.send(err);
+            }
+        })
     });
-});
 
 app.listen(3000, function (res, req) {
     console.log("Server is running on port 3000");
